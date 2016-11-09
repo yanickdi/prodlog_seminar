f18006bc9b4d790fd87c11b6e8deee20af6953e3
yanickdi <yanick.dickbauer@uni-graz.at>
2016-11-09 16:44:29 +0100

    solved

--- project.sh
+++
@@ -1,2 +0,0 @@
-python generate_data_file.py 25 1000
-ampl orientieering.run
---
+++ solve_alles.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ampl solve_BundeslaenderTour.run > Resultate/solve_BundeslaenderTour.txt
+ampl solve_OesterreichTourC500.run > Resultate/solve_OesterreichTourC500.txt
+ampl solve_OesterreichTourC1000.run > Resultate/solve_OesterreichTourC1000.txt
+ampl solve_OesterreichTourC1000AllProfits1.run > Resultate/solve_OesterreichTourC1000AllProfits1.txt
+ampl solve_OesterreichTourC15000.run > Resultate/solve_OesterreichTourC15000.txt
